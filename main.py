76d06928db362658d194c94610e4483ba12e820a
vidyadhari4 <54733357+vidyadhari4@users.noreply.github.com>
2020-10-03 13:56:14 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+from game import person, bcolors
+
+magic = [{"name": "Fire", "cost": 10, "dmg":60},
+         {"name": "Thunder", "cost": 10, "dmg":60},
+         {"name": "Blizzard", "cost": 10, "dmg":60}]
+
+player = person(460, 65, 60, 34, magic)
